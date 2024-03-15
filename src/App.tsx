d39b624593d37d52a0b9d26c3631b0d702cfefe4
Musa-Mabasa <musa14mabasa@gmail.com>
2024-03-15 18:18:48 +0200

    🎨 Removed returns

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import Reader from "./features/comicReader/Reader";
 import Home from "./features/homeFeature/Home";
-import MangaViewer from "./features/mangaViewer/MangaViewer";
-import Search from "./features/search/Search";
 
 const App = () => {
-  // return <Home />;
-
-  // return <MangaViewer/>
-
-  // return <Reader/>
-
-  return <Search />;
+  return <Home />;
 };
 
 export default App;
